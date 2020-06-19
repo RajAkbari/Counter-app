c59ce8d5a31ee81eeea4879c887de9a761491760
Raj Akbari <55597005+RajAkbari@users.noreply.github.com>
2020-06-19 07:25:17 +0530

    Update

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title: string = 'Counter App';
+
+  count: number = 0;
+
+  handleIncrease = () => {
+    if(this.count < 10){
+      this.count = this.count + 1;
+    }
+    else{
+      this.count;
+    }
+  }
+
+  handleDecrease = () => {
+    if(this.count > 0){
+      this.count = this.count - 1;
+    }
+    else{
+      this.count;
+    }
+  }
+
+  handleReset = () => {
+    this.count = 0;
+  }
+
+}
